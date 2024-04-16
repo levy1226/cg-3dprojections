98f71a90a314fecf1cbc42132884170ca9fbec80
nix05445 <133429874+nix05445@users.noreply.github.com>
2024-04-16 00:03:45 -0500

    Finished mat4x4Perspective() function in transform.js
    transform.js should now be complete

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -7,9 +7,53 @@ function mat4x4Perspective(prp, srp, vup, clip) {
     // 3. shear such that CW is on the z-axis
     // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
 
-    // ...
-    // let transform = Matrix.multiply([...]);
-    // return transform;
+    
+    let translatePRP = new Matrix(4, 4);
+    translatePRP.values = [[1, 0, 0, -(prp.x)],
+                           [0, 1, 0, -(prp.y)],
+                           [0, 0, 1, -(prp.z)],
+                           [0, 0, 0, 1]];
+
+                  
+    let n = prp.subtract(srp);
+    n.normalize();
+    let u = vup.cross(n); 
+    u.normalize();
+    let v = n.cross(u);
+
+    let CW = new Vector(3);
+    CW.x = (clip[0] + clip[1]) / 2;
+    CW.y = (clip[0] + clip[3]) / 2;
+    CW.z = -clip[4];
+
+    let DOP = new Vector(3);
+    DOP = CW;
+
+    let rotateVRC = new Matrix(4, 4);
+    rotateVRC.values = [[u.x, u.y, u.z, 0],
+                        [v.x, v.y, v.z, 0],
+                        [n.x, n.y, n.z, 0],
+                        [0, 0, 0, 1]];
+    
+    let shearCW = new Matrix(4, 4);
+    mat4x4ShearXY(shearCW, -DOP.x/DOP.z, -DOP.y/DOP.z);
+
+    let scalePer = new Matrix(4, 4);
+    let scaleX = (2 * clip[4])/((clip[1] - clip[0]) * clip[5]);
+    let scaleY = (2 * clip[4])/((clip[3] - clip[2]) * clip[5]);
+    let scaleZ = 1 / clip[5];
+    scalePer.values = [[scaleX, 0, 0, 0],
+                   [0, scaleY, 0, 0],
+                   [0, 0, scaleZ, 0],
+                   [0, 0, 0, 1]];
+
+    let nPer = Matrix.multiply([scalePer, shearCW, rotateVRC, translatePRP]);
+    let mPer = new Matrix(4, 4);
+    mPer.values = [[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, -1, 0]];
+    return Matrix.multiply([mPer, nPer]);
 }
 
 // create a 4x4 matrix to project a perspective image on the z=-1 plane
